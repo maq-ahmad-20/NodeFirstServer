c45c55589cf518081cf5d9fbcde52ebc334354b9
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-07-10 20:21:50 +0530

    cleanupcode

--- form.js
+++ form.js
@@ -1,52 +1,6 @@
 const http = require('http');
+const reqHandler = require('./routes');
 
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    if (url === '/') {
-        fs.readFile('message.txt', (err, data) => {
-            res.write('<html>')
-            res.write('<head><title>Enter Message</title></head>');
-
-            res.write(`<body><h1>${data}</h1><form action ="/message" method="POST"><input type ="text" name = "message"><button type ="submit">Submit</button></form></body>`)
-
-            res.write('</html>')
-            return res.end();
-
-        })
-
-
-
-    }
-    else if (url === '/message' && req.method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk)
-            console.log(chunk)
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody)
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, (err) => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-
-                return res.end();
-
-            });
-
-
-
-        })
-
-
-
-    }
-
-
-
-})
+const server = http.createServer(reqHandler.handler);
 
 server.listen(8000);

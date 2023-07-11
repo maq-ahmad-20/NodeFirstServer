c8fd2f5227956d7c206bacb65f1c2d1f884cc9e7
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-07-11 14:39:26 +0530

    consolingtherequestbodyaftersubmitingform

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use((req, res, next) => {
-    console.log('I am in middle ware')
-    next()
-})
-app.use((req, res, next) => {
-    console.log('I am in next middle ware')
-    res.send({ key1: "value" })
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-products', (req, res, next) => {
+
+    res.send('<form action="/products" method = "POST" ><input type="text" name="product" placeholder="Enter the item"><input type="number" name="quantity" placeholder="Enter the quantity"><button type="submit">AddProduct</button></form>')
+
+});
+
+app.use('/products', (req, res, next) => {
+    console.log('Enterded product : ' + req.body.product)
+    console.log('Selected quantity : ' + req.body.quantity)
+
+    res.redirect('/')
+});
+
+
+app.use('/', (req, res, next) => {
+
+    res.send('<h1>I am at Home Page</h1>')
+});
 
 app.listen(3000)

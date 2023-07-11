01a9c15eaf6056a77d941312dae0d19d8aba185a
maq-ahmad-20 <Maqboolahmad081@gmail.com>
2023-07-11 13:14:34 +0530

    ExpressFirstProgram

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-const http = require('http');
 
 
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    //console.log('Ahmad');
-    res.setHeader('Content-type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>firstPage</title></head>');
-    if (req.url === '/home') {
-        res.write('<p> Welcome home</p>');
-    } else if (req.url === '/about') {
-        res.write('<p> Welcome to About Us page</p>');
-    } else if (req.url === '/node') {
-        res.write('<p>Welcome to my Node Js project</p>');
-    }
-    res.write('</html>')
-    res.end();
+const app = express();
+app.use((req, res, next) => {
+    console.log('I am in middle ware')
+    next()
+})
+app.use((req, res, next) => {
+    console.log('I am in next middle ware')
+    res.send({ key1: "value" })
 })
 
-server.listen(4000);
-
-
+app.listen(3000)
--- form.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-const reqHandler = require('./routes');
-
-const server = http.createServer(reqHandler.handler);
-server.listen(8000);
